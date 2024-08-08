d180876357973679478cc1c037048279aea269ba
Oviyappriya <oviyappriya.a.s@gmail.com>
2024-08-08 15:16:46 +0530

    added changes

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -14,7 +14,7 @@ export default function ProfilePage() {
   }
 
   async function logout() {
-    await axios.post("https://vehicle-care-back-12.onrender.com/logout", { credential: "true" });
+    await axios.post("https://vehicle-care-back-12.onrender.com/logout");
     setRedirect("/");
     setUser(null);
   }
